ac1e9f7a3d9cbc05ac47e395aec30f052e1b9d43
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2017-10-06 21:59:27 +0700

    add SPHelper

--- app/src/main/java/ra/sumbayak/maverick/core/SharedPreferencesActivity.java
+++ app/src/main/java/ra/sumbayak/maverick/core/SharedPreferencesActivity.java
@@ -2,73 +2,75 @@ package ra.sumbayak.maverick.core;
 
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.support.annotation.*;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 
 public class SharedPreferencesActivity extends AppCompatActivity {
     
-    private SharedPreferences.Editor spEditor;
-    private SharedPreferences sp;
+    private SPHelper sp;
     
-    @Override
-    protected void onCreate (@Nullable Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
-        int spnameId = getResources ().getIdentifier ("spname", "string", getPackageName ());
-        String spname = getString (spnameId);
-        sp = getSharedPreferences (spname, MODE_PRIVATE);
+    public SPHelper getSPHelper () {
+        return sp;
     }
     
-    @SuppressLint ("CommitPrefEdits")
-    protected SharedPreferencesActivity putSP (String key, String value) {
-        if (spEditor == null)
-            spEditor = sp.edit ();
-        spEditor.putString (key, value);
-        return this;
-    }
-    
-    @SuppressLint ("CommitPrefEdits")
-    protected SharedPreferencesActivity putSP (String key, int value) {
-        if (spEditor == null)
-            spEditor = sp.edit ();
-        spEditor.putInt (key, value);
-        return this;
-    }
-    
-    @SuppressLint ("CommitPrefEdits")
-    protected SharedPreferencesActivity putSP (String key, boolean value) {
-        if (spEditor == null)
-            spEditor = sp.edit ();
-        spEditor.putBoolean (key, value);
-        return this;
-    }
-    
-    @SuppressLint ("CommitPrefEdits")
-    protected SharedPreferencesActivity putSP (@StringRes int stringId, String value) {
-        if (spEditor == null)
-            spEditor = sp.edit ();
-        spEditor.putString (getString (stringId), value);
-        return this;
+    protected class SPHelper {
         
-    }
-    @SuppressLint ("CommitPrefEdits")
-    protected SharedPreferencesActivity putSP (@StringRes int stringId, int value) {
-        if (spEditor == null)
-            spEditor = sp.edit ();
-        spEditor.putInt (getString (stringId), value);
-        return this;
-    }
-    
-    @SuppressLint ("CommitPrefEdits")
-    protected SharedPreferencesActivity putSP (@StringRes int stringId, boolean value) {
-        if (spEditor == null)
-            spEditor = sp.edit ();
-        spEditor.putBoolean (getString (stringId), value);
-        return this;
-    }
-    
-    protected void applySP () {
-        spEditor.apply ();
-        spEditor = null;
+        private SharedPreferences.Editor spEditor;
+        private SharedPreferences sp;
+        
+        SPHelper () {
+            int spnameId = getResources ().getIdentifier ("spname", "string", getPackageName ());
+            String spname = getString (spnameId);
+            sp = getSharedPreferences (spname, MODE_PRIVATE);
+        }
+        
+        public boolean containsSP (@StringRes int stringId) {
+            return containsSP (getString (stringId));
+        }
+        
+        public boolean containsSP (String key) {
+            return sp.contains (key);
+        }
+        
+        @SuppressLint ("CommitPrefEdits")
+        public SPHelper putSP (String key, String value) {
+            if (spEditor == null)
+                spEditor = sp.edit ();
+            spEditor.putString (key, value);
+            return this;
+        }
+        
+        @SuppressLint ("CommitPrefEdits")
+        public SPHelper putSP (String key, int value) {
+            if (spEditor == null)
+                spEditor = sp.edit ();
+            spEditor.putInt (key, value);
+            return this;
+        }
+        
+        @SuppressLint ("CommitPrefEdits")
+        public SPHelper putSP (String key, boolean value) {
+            if (spEditor == null)
+                spEditor = sp.edit ();
+            spEditor.putBoolean (key, value);
+            return this;
+        }
+        
+        public SPHelper putSP (@StringRes int stringId, String value) {
+            return putSP (getString (stringId), value);
+        }
+        
+        public SPHelper putSP (@StringRes int stringId, int value) {
+            return putSP (getString (stringId), value);
+        }
+        
+        protected SPHelper putSP (@StringRes int stringId, boolean value) {
+            return putSP (getString (stringId), value);
+        }
+        
+        public void applySP () {
+            spEditor.apply ();
+            spEditor = null;
+        }
     }
 }
